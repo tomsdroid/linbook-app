849e732b1bcd9a4c334cf729098f07a06ae190bf
LinBook <linbook@local>
2026-09-11 05:12:02 -0700

    Fix Android activity package crash

---
+++ android/app/src/main/java/com/linbook/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.linbook.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- android/app/src/main/java/com/mbaklin/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.mbaklin.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
